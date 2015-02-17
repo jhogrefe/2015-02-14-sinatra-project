03ca174cf8e9089b6e88279a1a53f90c5a4d3ffd
Jason Hogrefe <hogrefejason@gmail.com>
2015-02-17 08:08:03 -0600

    Added basic search field and linked to form erb file.

--- main.rb
+++ main.rb
@@ -5,8 +5,17 @@ require 'sqlite3'
 DATABASE = SQLite3::Database.new('database/localization.db')
 
 
-require_relative "database/database_setup"
+require_relative "database/database_setup.rb"
 require_relative "models/term.rb"
 require_relative "models/language.rb"
 require_relative "models/translation.rb"
 
+get "/" do
+  erb :main_search, :layout => :boilerplate
+end
+
+get "/translation" do 
+  t1 = Translation.new(params)  
+  t1.find
+  erb :translation, :layout => :boilerplate
+end
