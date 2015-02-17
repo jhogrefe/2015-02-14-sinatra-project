197ae59a2d54fe2979a2df49c257106fdc112a23
Jason Hogrefe <hogrefejason@gmail.com>
2015-02-17 07:43:47 -0600

    Updated test file. Need to add tests as features are added.

---
+++ tests/main_test.rb
@@ -0,0 +1,37 @@
+require "minitest/autorun"
+require "sqlite3"
+require 'pry'
+
+DATABASE = SQLite3::Database.new('../database/localization_test.db')
+
+
+require_relative "../database/database_setup"
+
+
+class LocalizationTest < Minitest::Test
+  
+  def setup
+    DATABASE.execute("DELETE FROM terms") # terms is the table name
+    DATABASE.execute("DELETE FROM languages") # languages is the table name
+    DATABASE.execute("DELETE FROM translations") # translations is the table name
+  end
+  
+  # def test_add_location
+  #   loc1 = Location.new({"location_name" => "TEST BUILDING"})
+  #   x = loc1.insert
+  #   assert_equal("TEST BUILDING", loc1.location_name)
+  # end
+  #
+  # def test_add_category
+  #   cat1 = Category.new({"manufacturer" => "BANANA"})
+  #   x = cat1.insert
+  #   assert_equal("BANANA", cat1.manufacturer)
+  # end
+  #
+  # def test_add_category_key
+  #   cat2 = Category.new({"manufacturer" => "BANANA"})
+  #   x = cat2.insert
+  #   assert_equal(1, cat2.id)
+  # end
+      
+end
