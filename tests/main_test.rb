dec66031b08c42ffb98ff9b834e9367cb717ed68
Jason Hogrefe <hogrefejason@gmail.com>
2015-02-23 13:16:54 -0600

    Added tests for term, language and translation insert methods.

--- tests/main_test.rb
+++ tests/main_test.rb
@@ -1,10 +1,12 @@
 require "minitest/autorun"
 require "sqlite3"
 require 'pry'
+require 'rubygems'
+require 'bing_translator'
 
 DATABASE = SQLite3::Database.new('../database/localization_test.db')
 
-
+require_relative "../helpers/iterate_over_language_array.rb"
 require_relative "../database/database_setup"
 require_relative "../models/term.rb"
 require_relative "../models/language.rb"
@@ -18,22 +20,22 @@ class LocalizationTest < Minitest::Test
     DATABASE.execute("DELETE FROM translations") # translations is the table name
   end
   
-  # def test_add_location
-  #   loc1 = Location.new({"location_name" => "TEST BUILDING"})
-  #   x = loc1.insert
-  #   assert_equal("TEST BUILDING", loc1.location_name)
-  # end
-  #
-  # def test_add_category
-  #   cat1 = Category.new({"manufacturer" => "BANANA"})
-  #   x = cat1.insert
-  #   assert_equal("BANANA", cat1.manufacturer)
-  # end
-  #
-  # def test_add_category_key
-  #   cat2 = Category.new({"manufacturer" => "BANANA"})
-  #   x = cat2.insert
-  #   assert_equal(1, cat2.id)
-  # end
+  def test_add_term
+    term1 = Term.new({"term" => "TEST TERM"})
+    x = term1.insert
+    assert_equal("TEST TERM", term1.term)
+  end
+
+  def test_add_language
+    lang1 = Language.new({"language" => "FRENCH"})
+    x = lang1.insert
+    assert_equal("FRENCH", lang1.language)
+  end
+
+  def test_add_translation
+    trans1 = Translation.new({"term_id" => 1, "language_id" => 1, "translation" => "FRENCH TERM"})
+    x = trans1.insert
+    assert_equal(1, trans1.id)
+  end
       
 end
