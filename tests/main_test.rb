22dec6d67b0a6bd519177d8cb56e24e474eada62
Jason Hogrefe <hogrefejason@gmail.com>
2015-02-24 07:48:38 -0600

    Added test for custom search using Microsoft Translator API.

--- tests/main_test.rb
+++ tests/main_test.rb
@@ -20,6 +20,12 @@ class LocalizationTest < Minitest::Test
     DATABASE.execute("DELETE FROM translations") # translations is the table name
   end
   
+  def setup
+    DATABASE.execute("DELETE FROM terms") # terms is the table name
+    DATABASE.execute("DELETE FROM languages") # languages is the table name
+    DATABASE.execute("DELETE FROM translations") # translations is the table name
+  end
+  
   def test_add_term
     term1 = Term.new({"term" => "TEST TERM"})
     x = term1.insert
@@ -36,6 +42,19 @@ class LocalizationTest < Minitest::Test
     trans1 = Translation.new({"term_id" => 1, "language_id" => 1, "translation" => "FRENCH TERM"})
     x = trans1.insert
     assert_equal(1, trans1.id)
+  end
+
+  def test_add_translation
+    cst1 = BingTranslator.new('free-software-translation',
+    '6njjbzRCq7rG3+CbFZzN+6jEV5ed63U9+2oqkJ9NuKo=', 'hello')
+      @es = cst1.translate('hello', :from => 'en', :to => 'es')
+      @fr = cst1.translate('hello', :from => 'en', :to => 'fr')
+      @de = cst1.translate('hello', :from => 'en', :to => 'de')
+      @it = cst1.translate('hello', :from => 'en', :to => 'it')
+      assert_equal(@es,'Hola')
+      assert_equal(@fr, 'Salut')
+      assert_equal(@de, 'Hallo')
+      assert_equal(@it, 'Ciao')      
   end
       
 end
