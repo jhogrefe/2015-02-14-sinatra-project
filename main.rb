f334a48f9fc3bb0d7e214ad4d1ac91faf0a5c7d1
Jason Hogrefe <hogrefejason@gmail.com>
2015-02-26 14:06:34 -0600

    Added new helper modules and require files.

--- main.rb
+++ main.rb
@@ -6,15 +6,17 @@ require 'bing_translator'
 
 DATABASE = SQLite3::Database.new('database/localization.db')
 
+require_relative "database/database_setup.rb"
 require_relative "controllers/index_controllers.rb"
 require_relative "controllers/search_controller.rb"
+require_relative "helpers/iterate_over_language_array.rb"
+require_relative "helpers/translate_using_db.rb"
+require_relative "helpers/translate_using_gem.rb"
 require_relative "controllers/language_controller.rb"
 require_relative "controllers/term_controller.rb"
 require_relative "controllers/translation_controller.rb"
-require_relative "helpers/iterate_over_language_array.rb"
-require_relative "database/database_setup.rb"
 require_relative "models/term.rb"
 require_relative "models/language.rb"
 require_relative "models/translation.rb"
 
-helpers GetLanguage
+helpers GetLanguage, TranslatorService, DatabaseTranslation
