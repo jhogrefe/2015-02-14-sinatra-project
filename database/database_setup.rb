5b3f88614be24eb15c4554013a642e04e80cb7fc
Jason Hogrefe <hogrefejason@gmail.com>
2015-03-16 08:50:54 -0500

    Added production configuration to database_setup.

--- database/database_setup.rb
+++ database/database_setup.rb
@@ -2,6 +2,18 @@ configure :development do
   set :database, {adapter: "sqlite3", database: "database/localization.db"}
 end
 
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'])
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+)
+end
+
 DATABASE = SQLite3::Database.new('database/localization.db')
 
 
