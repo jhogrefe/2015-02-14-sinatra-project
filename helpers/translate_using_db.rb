574c4ab9d72ca0174ac7890dafb233708890c069
Jason Hogrefe <hogrefejason@gmail.com>
2015-03-24 13:53:55 -0500

    Fixed url encoding issue giving me a space instead of % in search term params.

--- helpers/translate_using_db.rb
+++ helpers/translate_using_db.rb
@@ -1,5 +1,7 @@
 # Creates new translation object and uses searches database translations table 
 # to return translation.
+require "uri"
+
 module DatabaseTranslation
 
   def translate_using_db
@@ -9,7 +11,7 @@ module DatabaseTranslation
     else
       s1 = Term.where({term: params[:term].downcase})
       if s1[0] == nil
-        redirect to("/custom_search?term=#{params[:term]}")
+        redirect to("/custom_search?term=#{URI.escape(params[:term])}")
       else @term = (params[:term])
       end
       @s2 = Translator.where({term_id: (s1[0].id)})
